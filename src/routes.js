16314ffa0b6e320f134dc9e045b135f27da927c7
Jefferson de Lima Sousa <lima_jefferson@hotmail.com>
2021-08-19 15:37:19 -0300

    master

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,7 @@ const profile = {
   avatar: 'https://avatars.githubusercontent.com/u/58711245?v=4',
   'monthly-budget': 3000,
   'days-per-week': 5,
-  'hours-per-day': 5,
+  'hours-per-day': 6,
   'vacation-per-year': 4,
 };
 
